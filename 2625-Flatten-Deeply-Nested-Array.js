4e011cc5ebd97853301fbc04e247050cd1537559
Ragavan R V <167912221+Ragav1102@users.noreply.github.com>
2025-02-25 09:07:58 +0530

    [2625] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 2625-Flatten-Deeply-Nested-Array.js
@@ -0,0 +1,11 @@
+var flat = function (arr, n, res = []) {
+   
+    for(let i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i]) && n)
+            flat(arr[i], n - 1, res);
+        else
+            res.push(arr[i]);
+    }
+    return res;
+    
+};
